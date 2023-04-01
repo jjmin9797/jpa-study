376385699ad8cf8fd9db3a975342cdeac40419ef
jjmin9797 <jjmin9797@naver.com>
2023-04-01 22:23:42 +0900

    영속성 컨텍스트 공부

--- jpa-study-1/src/main/java/com/example/jpastudy1/hellojpa/JpaMain.java
+++ jpa-study-1/src/main/java/com/example/jpastudy1/hellojpa/JpaMain.java
@@ -1,7 +1,27 @@
 package com.example.jpastudy1.hellojpa;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 public class JpaMain {
-
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jm");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember = " + findMember.getName());
+            findMember.setName("JM");
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
 }
