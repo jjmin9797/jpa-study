52ab01dc8b75611889773ee2884610ded3557b6b
jjmin9797 <jjmin9797@naver.com>
2023-04-17 15:44:49 +0900

    feat: 엔티티 개발 완료

--- jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -26,4 +27,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,10 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Builder
 public class Member {
 
     @Id @GeneratedValue
